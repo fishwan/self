4065c216be89443f4bdd90ebf9d15e1eaea7dda2
yuchi <peaceyl4315@gmail.com>
2020-05-08 15:03:26 +0800

    300g1-分會網站

--- js/popup-bottom.js
+++
@@ -1,4 +0,0 @@
-// 置底廣告
-$(".popup-bottom-close").click(function(){
-    $(".popup-bottom").fadeOut();
-});
--- js/popup-cover.js
+++
@@ -1,9 +0,0 @@
-// 蓋版廣告
-$(".popup-cover").click(function(){
-    $(this).fadeOut();
-    $(".popup-cover__inner").fadeOut();
-});
-$(".popup-cover-close").click(function(){
-    $(".popup-cover").fadeOut();
-    $(".popup-cover__inner").fadeOut();
-});
