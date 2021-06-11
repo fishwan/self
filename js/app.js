30183db6dcb047b24dd70569eb78a476016d65b4
fishwan <peaceyl4315@gmail.com>
2021-06-11 10:07:52 +0800

    修改

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@
     const map = L.map('map',{
         zoomControl: false // 是否顯示預設的縮放按鈕（左上角）
     }).setView([24.8090765, 120.9709791], 16);
-    
-    // 紅色marker
-    const redIcon = new L.Icon({
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+
+    // 綠色marker
+    const greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        // 只要更改上面這一段的 green.png 成專案裡提供的顏色如：red.png，就可以更改 marker 的顏色
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
     });
-    const marker = L.marker([0, 0] , {icon:redIcon}).addTo(map);
+    const marker = L.marker([0, 0] , {icon:greenIcon}).addTo(map);
 
     var geocoder = L.Control.geocoder({
         defaultMarkGeocode: false
@@ -40,16 +41,7 @@
     let data;
     // 定義 marker 顏色
     let mask;
-    // 綠色marker
-    const greenIcon = new L.Icon({
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        // 只要更改上面這一段的 green.png 成專案裡提供的顏色如：red.png，就可以更改 marker 的顏色
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
+    
     // 橘色marker
     const orangeIcon = new L.Icon({
         iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
@@ -59,7 +51,15 @@
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
     });
-    
+    // 紅色marker
+    const redIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 
     // 執行 init
     init();
